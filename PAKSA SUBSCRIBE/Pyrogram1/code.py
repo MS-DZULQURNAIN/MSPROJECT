e6c4c5702fbacdba3db6f74d7c4314ac1c934e12
MS-DZULQURNAIN <121719789+MS-DZULQURNAIN@users.noreply.github.com>
2023-06-15 16:33:04 +0700

    Update code.py

--- PAKSA SUBSCRIBE/Pyrogram1/code.py
+++ PAKSA SUBSCRIBE/Pyrogram1/code.py
@@ -1,3 +1,13 @@
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-from pyrogram.errors.
+from pyrogram.errors.exceptions.bad_request_400 import UserNotParticipant
+
+@Bot.on_message(filters.incoming & filters.private, group=-1)
+async def fsub(bot, message):
+  if not FSUB:
+    return True
+  try:
+    await bot.get_chat_member(FSUB, message.from_user.id)
+  except UserNotParticipant:
+    if FSUB.startswith("-100"):
+      
